5dbe32f52bfe3377101557a0a9e2c5dff9c55c07
desKALtop <kalhelpert@gmail.com>
2024-08-23 18:55:54 -0300

    Alterado credenciais formato auth=user:pass em .env. Mongodb localhost banco: ticker

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const routes = require('./routes');
 const db = require('mongoose');
 require('dotenv').config();
 
+
 const app = express();
 const PORT =3000;
 const auth=(process.env.auth);
@@ -13,10 +14,39 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
+//Schema DB
+const acoesSchema = db.Schema({
+	ticker: String,
+	url:	String,
+	data: Date,
+	update: { type: Date, default: Date.now() },
+
+  });
+
+const acoes = db.model("acoes", acoesSchema);
+module.exports = acoes;
+
+
 // Routes
 app.use('/', routes);
 
-db.connect("mongodb+srv://"+auth+"@xndgdb.ywuzd.mongodb.net/?retryWrites=true&w=majority&appName=XNDGDB")
+
+app.post('/api/acoes', async (req, res) => {
+	try {
+		const acoes = await acoes.create(req.body);
+		res.status(200).json(acoes);
+
+	} catch (error) {
+		res.status(500).json({message: error.message});
+		
+	}
+
+} )
+
+//DB connection & start server
+//db.connect("mongodb+srv://"+auth+"@xndgdb.ywuzd.mongodb.net/ticker?retryWrites=true&w=majority&appName=XNDGDB")
+db.connect("mongodb://"+auth+"@localhost:27017/ticker?retryWrites=true&w=majority")
+
 	.then(() => {
 		
 		console.log("\x1b[1m\x1b[32m\x1b[5m", 'Sucesso!  Conectado ao DB!',"\x1b[0m");  //cyan
@@ -28,4 +58,9 @@ db.connect("mongodb+srv://"+auth+"@xndgdb.ywuzd.mongodb.net/?retryWrites=true&w=
 	.catch(() => {
 		console.log("\x1b[1m\x1b[31m\x1b[5m", 'Falha!   Conexão ao DB falhou',"\x1b[0m");
 	});
+
+
+
+
+
 
